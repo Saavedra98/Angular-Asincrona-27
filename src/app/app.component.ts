95d17cea2e8dd6ab7afb885ac7ff4a4cab44dbac
Saavedra98 <saavedraerazoc@gmail.com>
2023-01-27 09:36:23 -0500

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import swal from'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'asincrona-27';
+
+
+  formulario = new FormGroup({
+    usuarioControl: new FormControl('', [Validators.required, Validators.minLength(5)] ),
+    contraseñaControl: new FormControl('', [Validators.required, Validators.minLength(4)] ),
+  })
+
+
+  login(){
+    if(this.formulario.valid){
+      swal.fire({
+        icon: 'success',
+        title: 'Ingreso Correctamente',
+        showConfirmButton: false,
+
+      })
+    }else{
+      swal.fire({
+        icon: 'error',
+        title: 'Error al ingresar',
+        text: 'Llene correctamente los campos',
+      })
+    }
+
+  }
+
 }
